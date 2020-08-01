de5a3bcef0c0d8800510e0009905057ee3473c55
Barbaros KOCA <barbaroskoca@hotmail.com>
2020-08-01 23:46:59 +1000

    Structure

---
+++ src/polls/GuestName.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GuestName = () =>
+{
+    <div className="guestname-input">
+        <input type="text" placeholder="Your Name">
+        </input>
+    </div>
+}
+
+export default GuestName;
